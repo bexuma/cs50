86160955a394c319470f2b10dd32641cb469e8f8
Bexultan Myrzatayev <youngdeveloper.co@gmail.com>
2018-11-20 01:19:13 +0000

    style refactor

--- pset1/mario/more/mario.c
+++ pset1/mario/more/mario.c
@@ -7,28 +7,19 @@ int main(void)
     while (n > 23 || n < 1)
     {
         if (n == 0)
-        {
             return 0;
-        }
         n = get_int("Height: ");
     }
 
     for (int i = 1; i <= n; i++)
     {
         for (int w = 0; w < n - i; w++)
-        {
             printf(" ");
-        }
 
-        for (int h = 0; h < i; h++)
+        for (int h = 0; h < i * 2; h++)
         {
-            printf("#");
-        }
-
-        printf("  ");
-
-        for (int h = 0; h < i; h++)
-        {
+            if (h == i)
+                printf("  ");
             printf("#");
         }
 
